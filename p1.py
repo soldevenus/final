ced937b83475b8ecf8afaa06648ece07146de7f3
soldevenus <soldevenus@msn.com>
2016-04-21 21:12:36 -0300

    Initial commit

---
+++ p1.py
@@ -0,0 +1,18 @@
+def credit_card_penalty(balance_ days late):
+    result=0
+    if 15card_card penalty:
+        result balance 15*0.05
+    elif 30credit_card_penalty:
+        result= credit_ card_ penalty,30*0.10
+    elif 60 credit_card_penalty:
+        result=credit_card_penalty,60*0.15
+    elif 60 credit_card_penalty:
+        result=credit_card_penalty,60*0.20
+
+print "penalty 1:",credit_card_penalty(15000,18)
+
+print "penalty2:",credit_card_penalty(7000,31)
+
+print "penalty3:",credit_card_penalty(300,70)
+
+print"penalty4:",credit_card_penalty(1000,3)
---
+++ p2.py
@@ -0,0 +1,30 @@
+L=[9,-1,10,0,-2]
+x=19
+
+
+
+def positive_sum(L):
+    for n in L:
+        if n==x:
+
+
+print
+
+
+
+L=[-1,2,14,0,0,1]
+
+
+print"sum L:", positive_sum(L)
+
+
+M=[1,1,2,8,1]
+
+
+print "sum M:", positive_sum(M)
+
+N=[-1,-2,-3]
+
+
+print"sum N:",positive_sum(N)
+
